c828515e394b0b1f57b56c25c1db299839dc503d
Akshay Kumar <124715918+akshay030@users.noreply.github.com>
2025-06-07 23:26:50 +0530

    Srever.jsx fixed

--- server/server.js
+++ server/server.js
@@ -53,9 +53,15 @@ app.use("/api/auth",userRouter)
 app.use("/api/messages",messageRouter)
 
 await connectDB()
-const PORT = process.env.PORT || 5000
+//const PORT = process.env.PORT || 5000
 
-server.listen(PORT,()=>{
-    console.log("Server is running on Port" + PORT)
-})
 
+if(process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+
+export default server

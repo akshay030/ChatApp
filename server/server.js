302e8adb987162096790ca76edd6f80f666eb4a0
Akshay Kumar <124715918+akshay030@users.noreply.github.com>
2025-06-08 12:23:32 +0530

    fix server upload

--- server/server.js
+++ server/server.js
@@ -56,12 +56,11 @@ await connectDB()
 //const PORT = process.env.PORT || 5000
 
 
-if(process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000
-    server.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 export default server
